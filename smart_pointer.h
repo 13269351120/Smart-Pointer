562a6dcfd34746962c2af3a5675e87b23b15e429
Your Name <422619603@qq.com>
2019-03-29 15:33:57 +0800

    add * -> != ==

--- smart_pointer.h
+++ smart_pointer.h
@@ -2,6 +2,13 @@
 #define SMART_POINTER_H
 
 #include "ref_base.h"
+#define COMPARE(_op_)                          \
+bool operator _op_ (const SmartPointer& p ) {   \
+    return m_pointer _op_ p.m_pointer;          \
+}                                               \
+bool operator _op_ (const T* p ) {              \
+    return m_pointer _op_ p;                    \
+}                                               \
 
 template <typename T>
 class SmartPointer
@@ -31,6 +38,34 @@ class SmartPointer
             m_pointer = tmp;
             return *this;
         }
+        
+        T& operator* () const { return *m_pointer; }
+
+        T* operator-> () const { return m_pointer; }
+
+        /*
+        bool operator == (const T* p ) const
+        {
+            return m_pointer == p;
+        } 
+
+        bool operator == (const SmartPointer& other ) const 
+        {
+            return m_pointer == other.m_pointer;
+        }
+
+        bool operator != (const T* p ) const
+        {
+            return m_pointer != p;
+        } 
+
+        bool operator != (const SmartPointer& other ) const 
+        {
+            return m_pointer != other.m_pointer;
+        }
+        */
+        COMPARE (==);
+        COMPARE (!=);
 
         ~SmartPointer()
         {
